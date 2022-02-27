432b89e38f6b7803871d46798f5cfddb1603457e
Santiago Toledo <santiago.toledo@teamcmp.com>
2022-02-27 11:46:12 +0100

    WIP

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,36 +9,36 @@ DOCKER_COMPOSE="docker-compose -f ~/chia/docker-compose.yml"
 BKP1="${HOME}/chia/disks/chia-fd-1/chia-backup"
 BKP2="${HOME}/chia/disks/chia-fd-2/chia-backup"
 
-echo  "$(date) | ${GREEN}->Starting backup"
+echo  "$(date) | ${GREEN}-> Starting backup${NC}"
 
 if test -d "${BKP1}"; then
-  echo "$(date) | ${GREEN}-> Current backup found, rotating"
-  echo "$(date) | ${GREEN}--> Removing old backup"
+  echo "$(date) | ${GREEN}-> Current backup found, rotating${NC}"
+  echo "$(date) | ${GREEN}--> Removing old backup${NC}"
   rm -rf "${BKP2}"
-  echo "$(date) | ${GREEN}--> Moving ${BKP1} to ${BKP2}"
+  echo "$(date) | ${GREEN}--> Moving ${BKP1} to ${BKP2}${NC}"
   mv "${BKP1}" "${BKP2}"
 fi
 
-echo "$(date) | ${GREEN}-> Creating backup directory"
+echo "$(date) | ${GREEN}-> Creating backup directory${NC}"
 mkdir "${BKP1}"
 
-echo "$(date) | ${GREEN}-> Stopping the chia-farmer service"
+echo "$(date) | ${GREEN}-> Stopping the chia-farmer service${NC}"
 ${DOCKER_COMPOSE} stop chia
 
 sleep 10
 
-echo "$(date) | ${GREEN}-> Starting new backup"
+echo "$(date) | ${GREEN}-> Starting new backup${NC}"
 
-echo "$(date) | ${GREEN}--> Backing up the wallet db"
+echo "$(date) | ${GREEN}--> Backing up the wallet db${NC}"
 cp ~/chia/.chia/mainnet/wallet/db/blockchain_wallet_v1_mainnet_*.sqlite "${BKP1}/"
 
-echo "$(date) | ${GREEN}--> Backing up the blockchain db"
+echo "$(date) | ${GREEN}--> Backing up the blockchain db${NC}"
 cp ~/chia/.chia/mainnet/db/blockchain_v1_mainnet.sqlite "${BKP1}/"
 
-echo "$(date) | ${GREEN}-> Starting the chia-farmer service"
+echo "$(date) | ${GREEN}-> Starting the chia-farmer service${NC}"
 ${DOCKER_COMPOSE} start chia
 
-echo "$(date) | ${GREEN}-> Adding nodes to speed up the sync${NC}"
+echo "$(date) | ${GREEN}-> Adding nodes to speed up the sync${NC}${NC}"
 ${DOCKER_COMPOSE} exec -d bash /scripts/add-nodes.sh
 
-echo "$(date) | ${GREEN}-> Backup complete!"
+echo "$(date) | ${GREEN}-> Backup complete!${NC}"
